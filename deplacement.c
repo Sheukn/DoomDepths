9931e8a5c6f5e4ebe7b1e159dc7ef5de14472931
Sheukn <104200110+Sheukn@users.noreply.github.com>
2023-11-12 22:57:01 +0100

    Fix mouvement

--- deplacement.c
+++ deplacement.c
@@ -8,14 +8,14 @@ void moveHero(Cell** map, Position* pos, char move){
     switch (move){
         case 'w':
         case 'z':
-            if (map[pos->x-1][pos->y].type != '#'){
+            if (map[pos->x-1][pos->y].type != '#'  && pos->x-1 != 0){
                 map[pos->x][pos->y].type = ' ';
                 map[pos->x-1][pos->y].type = 'H';
                 pos->x--;
             }
             break;
         case 's':
-            if (map[pos->x+1][pos->y].type != '#'){
+            if (map[pos->x+1][pos->y].type != '#' && pos->x+1 != 9){
                 map[pos->x][pos->y].type = ' ';
                 map[pos->x+1][pos->y].type = 'H';
                 pos->x++;
@@ -23,14 +23,14 @@ void moveHero(Cell** map, Position* pos, char move){
             break;
         case 'a':
         case 'q':
-            if (map[pos->x][pos->y-1].type != '#'){
+            if (map[pos->x][pos->y-1].type != '#' && pos->y-1 != 0){
                 map[pos->x][pos->y].type = ' ';
                 map[pos->x][pos->y-1].type = 'H';
                 pos->y--;
             }
             break;
         case 'd':
-            if (map[pos->x][pos->y+1].type != '#'){
+            if (map[pos->x][pos->y+1].type != '#' && pos->y+1 != 9){
                 map[pos->x][pos->y].type = ' ';
                 map[pos->x][pos->y+1].type = 'H';
                 pos->y++;
